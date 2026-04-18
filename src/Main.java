a6d1120b381273f3855968d4ebebadc8f557edd7
TEYTEY4U <abubakaranozeumar3@gmail.com>
2026-04-18 19:45:11 +0100

    new changes

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Classwork {
+    public static void main(String[] args) {
+
+
+        Student s1 = new Student();
+        s1.display();
+
+        System.out.println("Is Passing: " + s1.isPassing());
+
+        // Using parameterized constructor
+        Student s2 = new Student("Abubakar", 101, "Computer Science", 50000, 85);
+        s2.display();
+
+        System.out.println("Is Passing: " + s2.isPassing());
+
+        // Update fee
+        s2.updateFee(55000);
+        s2.display();
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,55 @@
+public class Student {
+    String name;
+    int rollNumber;
+    String course;
+    double fee;
+    double marks;
+    char grade;
+
+    Student() {
+        name = "Unknown";
+        rollNumber = 0;
+        course = "Not Assigned";
+        fee = 0.0;
+        marks = 0.0;
+        grade = 'F';
+    }
+
+    Student(String studentName, int studentRollNumber, String studentCourse, double studentFee, double studentMarks) {
+        name = studentName;
+        rollNumber = studentRollNumber;
+        course = studentCourse;
+        fee = studentFee;
+        marks = studentMarks;
+        calculateGrade();
+    }
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Course: " + course);
+        System.out.println("Fee: " + fee);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + grade);
+    }
+    void calculateGrade() {
+        if (marks >= 75) {
+            grade = 'A';
+        } else if (marks >= 60) {
+            grade = 'B';
+        } else if (marks >= 50) {
+            grade = 'C';
+        } else if (marks >= 45) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+    }
+    void updateFee(double newFee) {
+        fee = newFee;
+        System.out.println("Fee updated successfully!");
+    }
+    boolean isPassing() {
+        return marks >= 50;
+    }
+
+}
