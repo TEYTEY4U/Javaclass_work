377b28a37ecf87575488324fa37d23401f6932c9
TEYTEY4U <68358067+TEYTEY4U@users.noreply.github.com>
2026-04-25 19:57:10 +0100

    MAIN2
    Comment out student object creation and display calls.

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,19 @@ public class Classwork {
     public static void main(String[] args) {
 
 
-        Student s1 = new Student();
-        s1.display();
+        //Student s1 = new Student();
+        //s1.display();
 
         System.out.println("Is Passing: " + s1.isPassing());
 
         // Using parameterized constructor
         Student s2 = new Student("Abubakar", 101, "Computer Science", 50000, 85);
-        s2.display();
+        System.out.printlm(s2);
 
-        System.out.println("Is Passing: " + s2.isPassing());
+        //System.out.println("Is Passing: " + s2.isPassing());
 
         // Update fee
-        s2.updateFee(55000);
-        s2.display();
+        //s2.updateFee(55000);
+        //s2.display();
     }
 }
